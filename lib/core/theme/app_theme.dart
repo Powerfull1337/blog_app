3cf6dc73cc4617772890cdafd8a2baeb4f99d129
Denys <den90021@gmail.com>
2025-03-07 14:08:22 +0200

    added basic app colors for test some feature

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:blog_app/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData darkThemeData = ThemeData(
+  scaffoldBackgroundColor: AppColors.backgroundColor,
+);
